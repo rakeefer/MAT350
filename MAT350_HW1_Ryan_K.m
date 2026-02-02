191ef7947a4d1b292f1d76c3fa0af9d96e676555
rakeefer <rakeefer@asu.edu>
2026-02-01 21:13:27 -0700

    Update MAT350_HW1_Ryan_K.m

--- MAT350_HW1_Ryan_K.m
+++ MAT350_HW1_Ryan_K.m
@@ -1,10 +1,11 @@
 
 Fs=8192; % Default sample rate.
 
-myfunction=sin((1:(3*Fs))*2*pi*(190/Fs))'; 
-% 3 seconds of sound with frequency f=190. 
+myfunction=sin((1:(3*Fs))*2*pi*(100/Fs))'; 
+% 3 seconds of sound with frequency f=100. 
 
 sound(myfunction); % Plays the function. Uses default sample rate.
 
 filename = 'myeee.wav'; % Make sure to know the folder where this is placed.
 audiowrite(filename,myfunction,Fs); % saves to file in "wav" format. 
+
